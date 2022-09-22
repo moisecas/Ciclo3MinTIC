8db3d7a326865f1477d957c4c7d3081a8884254f
Moisés <62753698+moisecas@users.noreply.github.com>
2022-09-21 20:55:40 -0500

    mvc supabase #2 inicio

---
+++ moiso/src/main/java/mvc/moiso/MoisoApplication.java
@@ -0,0 +1,13 @@
+package mvc.moiso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoisoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoisoApplication.class, args);
+	}
+
+}
---
+++ moiso/src/test/java/mvc/moiso/MoisoApplicationTests.java
@@ -0,0 +1,13 @@
+package mvc.moiso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoisoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
